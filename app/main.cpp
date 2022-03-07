e2086fa2dd13a3c9c1f9aa3ecf17e45fbcaea60e
Shaun Reed <shaunrd0@gmail.com>
2022-03-07 08:20:32 -0500

    Reorganize project files

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,7 @@
 /*##############################################################################
 ## Author: Shaun Reed                                                         ##
-## Legal: All Content (c) 2021 Shaun Reed, all rights reserved                ##
-## About: Main program for practice using Qt5 widgets and OpenGL              ##
+## Legal: All Content (c) 2022 Shaun Reed, all rights reserved                ##
+## About: Main program for practice using Qt6 widgets and OpenGL              ##
 ##                                                                            ##
 ## Contact: shaunrd0@gmail.com  | URL: www.shaunreed.com | GitHub: shaunrd0   ##
 ##############################################################################*/
