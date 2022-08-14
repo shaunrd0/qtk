44d9092306f6515641ae6587fd849e842ed3e1a1
Shaun Reed <shaunrd0@gmail.com>
2022-08-14 18:08:06 -0400

    Use git submodule for assimp

---
+++ cmake/include/git_submodule.cmake
@@ -0,0 +1,31 @@
+################################################################################
+## Author: Shaun Reed | Contact: shaunrd0@gmail.com | URL: www.shaunreed.com  ##
+##                                                                            ##
+## CMake function to update git submodules                                    ##
+################################################################################
+include_guard()
+
+find_package(Git)
+
+# _PATH: Path to git submodule location that we want to update
+# + submodule_update(extern/assimp)
+function(submodule_update _PATH)
+  if (NOT UPDATE_SUBMODULES)
+    return()
+  endif()
+
+  if (NOT GIT_FOUND)
+    message(FATAL_ERROR "Error: No git executable found")
+  endif()
+
+  message(STATUS "${_PATH}")
+  execute_process(
+      COMMAND ${GIT_EXECUTABLE} submodule update --init "${_PATH}"
+      WORKING_DIRECTORY "${CMAKE_SOURCE_DIR}"
+      RESULT_VARIABLE result
+  )
+
+  if (NOT result EQUAL 0)
+    message(FATAL_ERROR "Error: Unable to update git submodule at ${_PATH}")
+  endif()
+endfunction()
